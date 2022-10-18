7f9df3ed5d781fe5b139b7db2e81a3b7d7c3bd03
steelratty <steelratty@yandex.ru>
2022-10-18 08:47:28 +0300

    ДЗ по занятию Контейнеры и алгоритмы. ДЗ Исправление замечания

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -3,13 +3,14 @@ package homework;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
     //судя по тесту, нужен стек
-    private LinkedList<Customer> list = new LinkedList<>();
+    private Stack<Customer> list = new Stack<>();
 
     public void add(Customer customer) {
        list.push(customer);
--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -2,33 +2,26 @@ package homework;
 
 
 import java.util.*;
-import java.util.function.Consumer;
 
 public class CustomerService {
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
     private TreeMap<Customer, String>  map = new TreeMap<Customer, String>( Comparator.comparingLong(o -> o.getScores()) );
+    private Map.Entry copyEntry(Map.Entry<Customer, String> entry) {
+       if ( entry != null ) {
+           return new AbstractMap.SimpleEntry( new Customer( entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores() ), entry.getValue() );
+       }
+       else {
+           return null;
+       }
+    }
 
     public Map.Entry<Customer, String> getSmallest() {
-        Map.Entry<Customer, String> me = map.firstEntry();
-        Map.Entry me2 = null;
-        if (! (me ==null)) {
-            // нашли, создаем новую пару для возврата
-            me2 = new AbstractMap.SimpleEntry(new Customer(me.getKey().getId(), me.getKey().getName(), me.getKey().getScores()), me.getValue());
-        }
-        return me2;
+        return copyEntry(map.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer){
-      //  Map.Entry<Customer, String> me = Map.Entry.copyOf(map.ceilingEntry(customer));
-       // TreeMap <Customer, String>  map2 = (TreeMap<Customer, String>) map.clone();
-        Map.Entry<Customer, String> me = map.higherEntry(customer);
-        Map.Entry me2 = null;
-        if (! (me ==null)) {
-            // нашли, создаем новую пару для возврата
-            me2 = new AbstractMap.SimpleEntry(new Customer(me.getKey().getId(), me.getKey().getName(), me.getKey().getScores()), me.getValue());
-        }
-        return me2;
+        return copyEntry(map.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
