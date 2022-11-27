2c2826ce29cefe09dcd540df6aaf5ce399ad5eb2
steelratty <steelratty@yandex.ru>
2022-11-27 16:29:02 +0300

    hw07

---
+++ hw07/src/main/java/ru/otus/model/ObjectForMessage.java
@@ -0,0 +1,16 @@
+package ru.otus.model;
+
+import java.util.List;
+
+public class ObjectForMessage {
+    private List<String> data;
+
+    public List<String> getData() {
+        return data;
+    }
+
+    public void setData(List<String> data) {
+        this.data = data;
+    }
+
+}
