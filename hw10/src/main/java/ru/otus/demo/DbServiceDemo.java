7807e20b8b1d32dfa779fa17aa3a7ea12f3823dc
steelratty <steelratty@yandex.ru>
2023-01-04 13:56:37 +0300

    hw10

---
+++ hw10/src/main/java/ru/otus/demo/DbServiceDemo.java
@@ -0,0 +1,21 @@
+package ru.otus.demo;
+
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.core.repository.DataTemplateHibernate;
+import ru.otus.core.repository.HibernateUtils;
+import ru.otus.core.sessionmanager.TransactionManagerHibernate;
+import ru.otus.crm.dbmigrations.MigrationsExecutorFlyway;
+import ru.otus.crm.model.Address;
+import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Phone;
+import ru.otus.crm.service.DbServiceClientImpl;
+
+public class DbServiceDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(DbServiceDemo.class);
+
+    public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+
+}
